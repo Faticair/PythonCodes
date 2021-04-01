63dbc2986672d721e10ba6ee36ea6275c6354dac
Zahi <1737453862@qq.com>
2021-04-01 10:01:53 +0800

    update diff_csv related files

--- ZahiTool/diff_csv.py
+++ ZahiTool/diff_csv.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import datetime
 
 def read_csv_file(csvpath):
     hashdic = {}
@@ -23,13 +24,16 @@ def is_list_diff(oldlist, newlist):
 
 def item_print(printmode, items, referdic):
     for index in items:
-        print('\n### ' + printmode + ' item: ' + format_value(referdic.pop(index)))
+        # print('\n### ' + printmode + ' item: ' + format_value(referdic.pop(index)))
+        mytext = '\n\n### ' + printmode + ' item: ' + format_value(referdic.pop(index))
+        output_to_file(mytext)
 
 def format_value(originvalue):
     return str(originvalue).replace('\\t', '')
 
-def output_to_file(filepath, text):
-    with open(filepath, 'a', encoding='utf-8') as f:
+def output_to_file(text):
+    filepath = 'D:\\Files\\MyJob\\Wireless\\matter\\diff_' + datetime.date.today().isoformat() + '.txt'
+    with open(filepath, 'a+', encoding='utf-8') as f:
         f.write(text)
 
 def file_compare(Oldfile, Newfile):
@@ -38,29 +42,39 @@ def file_compare(Oldfile, Newfile):
     increaseitem = set_diff(set(nidlist), set(oidlist))
     decreaseitem = set_diff(set(oidlist), set(nidlist))
     if not increaseitem:
-        print('### NO new item.')
+        # print('### NO new item.')
+        output_to_file('\n\n### NO new item.')
     else:
         item_print('New', increaseitem, nhashdic)
 
     if not decreaseitem:
-        print('### NO item miss.')
+        # print('### NO item miss.')
+        output_to_file('\n\n### NO item miss.')
     else:
         item_print('Miss', decreaseitem, ohashdic)
-    print('\n### Different item: ')
+    # print('\n### Different item: ')
+    output_to_file('\n\n### Different item: ')
     count = 0
     for key in nhashdic.keys():
         if is_list_diff(ohashdic[key], nhashdic[key]):
             count += 1
-            print('\n### ' + str(count))
-            print('### New: ' + format_value(nhashdic[key]))
-            print('### Old: ' + format_value(ohashdic[key]))
-
+            # print('\n### ' + str(count))
+            # print('### New: ' + format_value(nhashdic[key]))
+            # print('### Old: ' + format_value(ohashdic[key]))
+            mytext = '\n\n### ' + str(count) + '\n### New: ' + format_value(nhashdic[key]) + '\n### Old: ' + format_value(ohashdic[key])
+            output_to_file(mytext)
 def main():
     rootpath = 'D:\\Files\\MyJob\\Wireless\\matter\\'
     oldfile = 'AC6805-apInfo-0322-all.csv'
     newfile = 'AC6805-apInfo-0329-all.csv'
+    mytext = '###################\nold file: ' + oldfile + '\nnew file: ' + newfile + '\n###################'
+    output_to_file(mytext)
     file_compare(rootpath + oldfile, rootpath + newfile)
     
 
 if __name__ == '__main__':
+    start_time = datetime.datetime.now()
     main()
+    end_time = datetime.datetime.now()
+    runningtime = (end_time - start_time)
+    print('Task finished. Time used: ' + str(runningtime))
